6dd052ca28e7d1b5fdef5d7a56e7bda8e50bccb9
dkaramit <dkaramit@yahoo.com>
2022-07-09 14:26:48 +0100

    testing

--- test.cpp
+++ test.cpp
@@ -12,8 +12,14 @@ int main(){
 
     std::map at=sad::at(vector{x},vector{0.}) ;
 
+    /*exp(log(x)) is simplified to x*/
+    sad::Expression f0=exp(log(x));
+    sad::Expression f1=derivative(exp(log(x)),x);
+    sad::Expression f2=derivative(log(exp(x)),x);
 
-    cout<< evaluate(derivative(exp(log(x)),x),at)  <<"\n";
+    cout<< sad::print_expr(f0)<< "\t" << evaluate(f0,at)  <<"\n";
+    cout<< sad::print_expr(f1)<< "\t" << evaluate(f1,at)  <<"\n";
+    cout<< sad::print_expr(f2)<< "\t" << evaluate(f2,at)  <<"\n";
 
     return 0;
 }
